cc1b07ad6fba0d976af5b0447d2dd7380505a229
Hung Nguyen Dang <hungnguyendang@Hungs-MacBook-Pro.local>
2018-07-12 16:42:22 +0100

    udpate config message broker with kafka stream

---
+++ src/main/java/io/github/crawlerbot/messaging/Greeting.java
@@ -0,0 +1,17 @@
+package io.github.crawlerbot.messaging;
+
+public class Greeting {
+    private String message;
+
+    public Greeting() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Greeting setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+}
